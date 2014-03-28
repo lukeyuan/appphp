019bffefb5df4948be02551709b5ee8777712ff5
lukeyuan <yuanlukeyuan@gmail.com>
2014-03-28 16:33:45 +0800

    add news
    add news

--- php/OpenshiftWeChatHttpRequest.php
+++ php/OpenshiftWeChatHttpRequest.php
@@ -25,16 +25,44 @@
 		}
 		require_once('sql.php');		//引入数据库接口
 		require_once('logic.php');		//引入逻辑模块
+		require_once('statemachine.php');
 		$logic = new Logic();
 		$result = $logic->all($para);
-		$textTpl = "<xml>
-						<ToUserName><![CDATA[%s]]></ToUserName>
-						<FromUserName><![CDATA[%s]]></FromUserName>
-						<CreateTime>%s</CreateTime>
-						<MsgType><![CDATA[%s]]></MsgType>
-						<Content><![CDATA[%s]]></Content>
-						<FuncFlag>0</FuncFlag>
-					</xml>";	
-	   	echo sprintf($textTpl, $para['FromUserName'], $para['ToUserName'], time(), $result['MsgType'], $result['Content']);
+		$msgType = $result['MsgType'];
+
+		if($msgType == 'text') {		//回复文本消息
+			$textTpl = "<xml>
+							<ToUserName><![CDATA[%s]]></ToUserName>
+							<FromUserName><![CDATA[%s]]></FromUserName>
+							<CreateTime>%s</CreateTime>
+							<MsgType><![CDATA[text]]></MsgType>
+							<Content><![CDATA[%s]]></Content>
+							<FuncFlag>0</FuncFlag>
+						</xml>";	
+		   	echo sprintf($textTpl, $para['FromUserName'], $para['ToUserName'], time(), $result['Content']);			
+		}
+		else if($msgType == 'news') {		//回复图文消息
+			$temp = "<xml>
+					<ToUserName><![CDATA[%s]]></ToUserName>
+					<FromUserName><![CDATA[%s]]></FromUserName>
+					<CreateTime>%s</CreateTime>
+					<MsgType><![CDATA[news]]></MsgType>
+					<ArticleCount>%s</ArticleCount>
+					<Articles>";
+			$textTpl = sprintf($temp, $para['FromUserName'], $para['ToUserName'], time(), $result['ArticleCount']);
+			$articleCount = $result['ArticleCount'];			
+			for ($i = 1; $i <= $articleCount; $i++) { 
+				$temp = "<item>
+							<Title><![CDATA[%s]]></Title> 
+							<Description><![CDATA[%s]]></Description>
+							<PicUrl><![CDATA[%s]]></PicUrl>
+							<Url><![CDATA[%s]]></Url>
+						</item>";
+				$textTpl .= sprintf($temp, $result['Title' . "$i"], $result['Description' . "$i"], $result['PicUrl' . "$i"], $result['Url' . "$i"]);
+			}
+			$textTpl .= "</Articles></xml>";
+			echo $textTpl;
+		}
+
 	}
 ?>
